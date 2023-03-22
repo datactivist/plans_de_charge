95535b5822617ab071533bb7515dc6763dd98c20
DianeThy <diane.thierry.3asd@gmail.com>
2023-03-22 17:45:49 +0100

    dataviz calendrier et barplot

--- dataviz_odoo.R
+++ dataviz_odoo.R
@@ -19,13 +19,15 @@ data <- data |> rename(taches = Titre,
                        assigne_sous_tache = `Sous-tâches/Assigné à`, 
                        etat = `Étiquette d'état Kanban`)
 diane <- data |> filter(assigne == "Diane Thierry")
+pauline <- data |> filter(assigne == "Pauline Breton-Chauvet")
+pauline <- data |> filter(assigne == "Anne-Laure Donzel")
 
 
 
-# Visualisation
+# Visualisation n°1
 #graph <- 
 library(plotly)
-diane |>
+graph <- pauline |> filter(!is.na(debut)) |> 
     ggplot() +
     geom_segment(aes(
         x = debut,
@@ -33,24 +35,80 @@ diane |>
         y = taches,
         yend = taches,
         lwd = heures_prevues,
-        #fill = Projet
+        col = heures_prevues
     )) +
-    # facet_grid(Projet ~ ., 
-    #            scales = "free",
-    #            space = "free"
-    #            ) +
-    facet_wrap(~Projet, 
-               scales = "free",
-               #space = "free",
-               ) +
+    ggforce::facet_col(facets = vars(Projet), 
+                     scales = "free_y", 
+                     space = "free") +
     scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 70)) +
+    scale_color_continuous(high = "#132B43", low = "#56B1F7") +
     theme_bw() +
-    theme(strip.text.y = element_text(angle = 0))
+    # theme(#strip.text.y = element_text(angle = 0),
+    #       strip.background = element_blank(),
+    #       strip.text.y = element_blank()) +
+    guides(lwd = "none") +
+    geom_vline(aes(xintercept = as.numeric(as.POSIXct(Sys.Date()))), 
+               col = "red", size = .35)
+
 ggplotly(graph) %>% 
     layout(legend = list(orientation = "h", x = 0.4, y = -0.2),
-           showlegend = FALSE)
-library(ggiraph)
-girafe(graph)
+           showlegend = FALSE) 
+
+
+# Visualisation n°2
+
+# Data
+cumul_projet <- pauline |> 
+    filter(etat != "Pret",
+           Projet != "Tâches non facturables à timesheeter") |> 
+    group_by(Projet) |> 
+    mutate(hprevues = sum(heures_prevues),
+           hpassees = sum(heures_passees),
+           hrestantes = sum(heures_restantes),
+           nb_taches = n()) |> 
+    ungroup() |> 
+    distinct(Projet, hprevues, hpassees, hrestantes, debut, fin, nb_taches) |> 
+    pivot_longer(cols = c(hprevues, hpassees, hrestantes), 
+                 names_to = "periode", 
+                 values_to = "heures", 
+                 names_prefix = "h") |> 
+    mutate(periode = case_when(periode == "passees" ~ "réalisées",
+                              periode == "prevues" ~ "prévues",
+                              TRUE ~ periode),
+           periode = factor(periode, ordered = T, levels = c("restantes", "réalisées", "prévues")),
+           Projet = reorder(Projet, heures, desc = T))
+
+
+# Graph
+cumul_projet |> 
+    ggplot(aes(x=Projet, y = heures, fill = periode, group = periode)) +
+    geom_bar(stat="identity", position = "dodge", width=.6, col = "white", size = .2) +
+    theme_classic() +
+    coord_flip() +
+    labs(title = "Heures prévues, réalisées et restantes des missions", 
+         y = "Nombre d'heures totales", x = "") +
+    scale_fill_manual(values = c("réalisées" = "#ee7440", 
+                                 "prévues" = "#3368cf",
+                                 "restantes" = "#18ba13")) +
+    scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 70)) +
+    guides(fill = guide_legend(title = "Heures", reverse = TRUE))
+ggplotly(graph)
+
+
+graph <- cumul_projet %>% #mutate(title_projet = reorder(title_projet, n, desc=T)) %>% 
+    #filter(n > 50) %>% 
+  ggplot(aes(x=Projet, y = n, fill = Genre, group = Genre, text = paste(part, "%")))+
+    geom_bar(stat="identity", position = "dodge", width=.6, col = "white", size = .2) +
+    labs(x = "", y = "Nombre d'utilisateurs", 
+         title = paste0("Genre des contributeurs par projet de plus de 50 contributions\n(", (table %>% filter(!is.na(gender)) %>% summarise(n=n()))$n, " genres connus sur ", nrow(table), " contributions)", sep = "")) +
+    theme_classic() +
+    coord_flip() +
+    scale_fill_manual(values = c("#da4729", "#21468d")) +
+    theme(legend.position = "right",
+        text = element_text(family = "Montserrat", size = 12),
+        plot.title = element_text(hjust = 1)) +
+    guides(fill = guide_legend(title = "", reverse = TRUE))
+ggplotly(graph, tooltip = c("text"))
 
 
 
